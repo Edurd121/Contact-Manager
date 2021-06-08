4125fa3071de16677479f15e2185cc5364f241e9
john <john.n.pham2000@gmail.com>
2021-06-07 22:58:39 -0400

    attempting to display contacts via getelementbyid function

--- js/code.js
+++ js/code.js
@@ -208,8 +208,9 @@ function searchContact() {
 				}
 				// console.log(JSON.parse(jsonObject));
 				console.log(jsonObject);
-				contactList = displayContacts(jsonObject);
+				displayContacts(jsonObject)
 				document.getElementsByTagName("p")[0].innerHTML = contactList;
+
 			}
 		};
 		xhr.send(jsonPayload);
@@ -222,16 +223,16 @@ function searchContact() {
 }
 
 function displayContacts(contacts) {
-	contacts.results.map((contact) => {
+	document.getElementById("contactsList").innerHTML =	contacts.results.map((contact) => {
 		let temp = contact.split(", ");
-		'<div class="card text-dark bg-light mb-6" style="max-width: 18rem;">'+
-			'<div class="card-header">'+temp[0]+'</div>'+
-			'<div class="card-body">'+
-				'<p class="card-title">'+temp[1]+'</p>'+
-				'<p class="card-text">'+temp[2]+'</p>'+
-			'</div>'+
-		'</div>'
-		
+		`<div class="card text-dark bg-light mb-6" style="max-width: 18rem;">
+			<div class="card-header">${temp[0]}</div>
+				<div class="card-body">
+					<p class="card-title">${temp[1]}</p>
+					<p class="card-text">${temp[2]}</p>
+				</div>
+			</div>
+		</div>`
 	})
 }
 
